5aeb4d3dbd83292cb004622a8d1d8f297d3b0002
khoa <khoadd879@gmail.com>
2025-12-05 17:23:25 +0700

    fix logic

--- routers/fileEndpoint.py
+++ routers/fileEndpoint.py
@@ -3,7 +3,7 @@ import fastapi
 from fastapi.security import HTTPBearer
 from database.database import SessionLocal
 from dependencies.auth import require_auth
-from fastapi import UploadFile, File, Depends
+from fastapi import HTTPException, UploadFile, File, Depends
 from pathlib import Path
 from sqlalchemy.orm import Session
 from models import FileUpload
@@ -14,6 +14,7 @@ security = HTTPBearer()
 
 UPLOAD_FOLDER = Path("uploads")
 UPLOAD_FOLDER.mkdir(exist_ok=True) 
+MAX_SIZE = 50 * 1024 * 1024 #50MB
 
 def get_db():
     db = SessionLocal()
@@ -25,6 +26,12 @@ def get_db():
 @router.post("/upload", response_model=None)
 async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
     file_path = UPLOAD_FOLDER / file.filename
+    
+    contents = await file.read()  
+    
+    if len(contents) > MAX_SIZE:
+        raise HTTPException(status_code=413, detail="File too large. Max 50MB allowed.")
+    
     
     mimetype = file.content_type
     maintype = mimetype.split("/")[0]
